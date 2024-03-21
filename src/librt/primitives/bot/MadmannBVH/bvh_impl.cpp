64d126bce9ea4de4e121aff45a54e687a85d3907
Gaultier Delbarre <gaultiertexas@gmail.com>
2024-03-21 15:37:13 -0500

    That'll do, pig.

--- src/librt/primitives/bot/MadmannBVH/bvh_impl.cpp
+++ src/librt/primitives/bot/MadmannBVH/bvh_impl.cpp
@@ -1,7 +1,6 @@
 #include "bvh_impl.h"
 
 #include <bits/chrono.h>
-#include <chrono>
 #include <initializer_list>
 #include <iomanip>
 #include <iostream>
@@ -13,25 +12,25 @@
 #include "rt/primitives/bot.h"
 #include "rt/rt_instance.h"
 #include "rt/tie.h"
-#include "bio.h"
 #include "rt/geom.h"
 #include "rt/seg.h"
+
+#include "bu/parallel.h"
 #include "common.h"
-#include "src/bbox.h"
-#include "src/executor.h"
-#include "src/thread_pool.h"
-#include "src/vec.h"
 #include "vmath.h"
+#include "../../../librt_private.h"
 
 #include "src/bvh.h"
 #include "src/default_builder.h"
 #include "src/tri.h"
 #include "src/stack.h"
-#include "../../../librt_private.h"
+#include "src/bbox.h"
+#include "src/executor.h"
+#include "src/thread_pool.h"
+#include "src/vec.h"
 
-#include "bu/parallel.h"
 
-#define PERMUTE_PRIMS true
+#define PERMUTE_PRIMS false
 
 namespace BVH = bvh::v2;
 
@@ -70,6 +69,15 @@ struct Accel {
   Bvh bvh;
   std::vector<PrecomputedTri> tris;
   std::vector<Tri> ptrTris;
+
+  bvh::v2::SmallStack<typename Bvh::Index, 128 > * stacks;
+
+  Accel()
+    : stacks( new std::decay_t<decltype(*stacks)>[ bu_avail_cpus() ] ) {}
+
+  ~Accel() {
+    delete[] stacks;
+  }
 };
 
 /**
@@ -129,17 +137,16 @@ int bvh_build( struct soltab *stp, struct rt_bot_internal *bot_ip, struct rt_i *
   // build_options.min_primitives_for_parallel_build = 0;
   // nanort::BVHAccel<Float> * accel = new nanort::BVHAccel<Float>();
   typename BVH::DefaultBuilder<Node>::Config config;
-  config.quality = BVH::DefaultBuilder<Node>::Quality::Low;
+  config.quality = BVH::DefaultBuilder<Node>::Quality::High;
 
 
-  BVH::ThreadPool threadpool( 1 );
-  // BVH::ThreadPool threadpool( bu_avail_cpus() );
+  // BVH::ThreadPool threadpool( 1 );
+  BVH::ThreadPool threadpool( bu_avail_cpus() );
   BVH::ParallelExecutor executor( threadpool );
 
   BBox model_bbox = BBox::make_empty();
   std::vector<BBox> bboxes( bot_ip->num_faces );
   std::vector<Vec3> centers( bot_ip->num_faces );
-  std::vector<PtrTri> tris( bot_ip->num_faces );
 
   std::cout << "Made threadpool and executor!" << std::endl;
 
@@ -153,48 +160,24 @@ int bvh_build( struct soltab *stp, struct rt_bot_internal *bot_ip, struct rt_i *
   accel.ptrTris.resize( bot_ip->num_faces );
   accel.tris.resize( bot_ip->num_faces );
 
-  for( unsigned i = 0; i < bot_ip->num_faces; ++i ) {
-    auto face0 = bot_ip->faces[i + 0];
-    auto face1 = bot_ip->faces[i + 1];
-    auto face2 = bot_ip->faces[i + 2];
-    Float * v0 = bot_ip->vertices + face0;
-    Float * v1 = bot_ip->vertices + face1;
-    Float * v2 = bot_ip->vertices + face2;
-    PtrTri tri( v0, v1, v2 );
+  // Face Array
+  // [v0] [v1] [v2] [v0]
+  executor.for_each( 0, bot_ip->num_faces, [&](size_t start, size_t end) {
+    for( unsigned i = start; i < end; ++i ) {
+      auto face0 = 3 * bot_ip->faces[3*i + 0];
+      auto face1 = 3 * bot_ip->faces[3*i + 1];
+      auto face2 = 3 * bot_ip->faces[3*i + 2];
+      Float * v0 = bot_ip->vertices + face0;
+      Float * v1 = bot_ip->vertices + face1;
+      Float * v2 = bot_ip->vertices + face2;
+      PtrTri tri( v0, v1, v2 );
 
-    std::cerr << "Triangle ( face IDX " << i << " / " << bot_ip->num_faces << " )" << std::endl;
-    std::cerr << "\tV0: " << vecString( v0 ) << std::endl;
-    std::cerr << "\tV1: " << vecString( v1 ) << std::endl;
-    std::cerr << "\tV2: " << vecString( v2 ) << std::endl;
-
-    centers[i] = tri.get_center();
-    bboxes[i] = tri.get_bbox();
-    model_bbox.extend( bboxes[i] );
-    accel.ptrTris[i] = std::move( tri );
-    accel.tris[i] = accel.ptrTris[i];
-    accel.tris[i].prim_id = i;
-  }
-  //executor.for_each( 0, bot_ip->num_faces, [&bot_ip, &centers, &bboxes, &tris, &model_bbox, &accel, vecString](size_t begin, size_t end) -> void {
-  //  for( unsigned i = begin; i < end; ++i ) {
-  //    auto face = bot_ip->faces[i];
-  //    auto tri_idx = face * 3 * 3;
-  //    Float * v0 = bot_ip->vertices + tri_idx;
-  //    Float * v1 = bot_ip->vertices + tri_idx + 3;
-  //    Float * v2 = bot_ip->vertices + tri_idx + 6;
-  //    PtrTri tri( v0, v1, v2 );
-
-  //    std::cerr << "Triangle " << face << ": ( face IDX " << i << " / " << bot_ip->num_faces << " )" << std::endl;
-  //    std::cerr << "\tV0: " << vecString( v0 ) << std::endl;
-  //    std::cerr << "\tV1: " << vecString( v1 ) << std::endl;
-  //    std::cerr << "\tV2: " << vecString( v2 ) << std::endl;
-
-  //    centers[i] = tri.get_center();
-  //    bboxes[i] = tri.get_bbox();
-  //    model_bbox.extend( bboxes[i] );
-  //    accel.ptrTris[i] = std::move( tri );
-  //  }
-  //});
-
+      centers[i] = tri.get_center();
+      bboxes[i] = tri.get_bbox();
+      model_bbox.extend( bboxes[i] );
+      accel.ptrTris[i] = std::move( tri );
+    }
+  });
 
   std::cout << "Bounding boxes and centers calculated" << std::endl;
 
@@ -204,59 +187,24 @@ int bvh_build( struct soltab *stp, struct rt_bot_internal *bot_ip, struct rt_i *
   std::cerr << "Num faces: " << bot_ip->num_faces << std::endl;
 
   // Precompute Triangles...
-  // accel.tris.resize( bot_ip->num_faces );
-  // executor.for_each( 0, bot_ip->num_faces, [&accel, &bot_ip, &tris]( size_t begin, size_t end ) {
-  //   for( int i = begin; i < end; i++ ) {
-  //     int j;
-  //     if constexpr ( PERMUTE_PRIMS ) {
-  //       j = bot_ip->faces[i];
-  //     }
-  //     else {
-  //       j = i;
-  //     }
+  executor.for_each( 0, bot_ip->num_faces, [&](size_t begin, size_t end) {
+    for( int i = 0; i < bot_ip->num_faces; ++i ) {
+      int j = PERMUTE_PRIMS ? accel.bvh.prim_ids[i] : i;
+      accel.tris[i] = accel.ptrTris[j];
+      accel.tris[i].prim_id = j;
+    }
+  });
 
-  //     accel.tris[i] = accel.ptrTris[i];
-  //     accel.tris[i].prim_id = j;
-  //   }
-  // });
 
   std::cerr << "Triangles Computed!" << std::endl;
 
-
-  // std::cerr << "Building triangle mesh and pred..." << std::endl;
-  // nanort::TriangleMesh<Float> triangle_mesh( bot_ip->vertices, (const unsigned *) bot_ip->faces, sizeof(fastf_t)*3 );
-  // nanort::TriangleSAHPred<Float> triangle_pred( bot_ip->vertices, (const unsigned *) bot_ip->faces, sizeof(fastf_t)*3 );
-
-
-  // std::cerr << "Building accelerator..." << std::endl;
-  // std::cerr << bot_ip->num_faces * 3 << " triangles" << std::endl;
-  // auto start_time = std::chrono::system_clock::now().time_since_epoch();
-  // auto ret = accel->Build( bot_ip->num_faces, triangle_mesh, triangle_pred, build_options );
-  // auto end_time = std::chrono::system_clock::now().time_since_epoch() - start_time;
-  // std::cerr << "Accelerator built! Took " << std::chrono::duration_cast< std::chrono::milliseconds >( end_time ).count() << " ms " << std::endl;
-  // nanort::BVHBuildStatistics stats = accel->GetStatistics();
-
-  // if( not ret ) throw std::runtime_error("Failed to build NanoRT BVH!");
-
-  // printf("  BVH statistics:\n");
-  // printf("    # of leaf   nodes: %d\n", stats.num_leaf_nodes);
-  // printf("    # of branch nodes: %d\n", stats.num_branch_nodes);
-  // printf("  Max tree depth     : %d\n", stats.max_tree_depth);
-
   // // Set the min and max bounding boxes.
-  // accel->BoundingBox( stp->st_min, stp->st_max );
-  // printf("  BVH Bounding Box Min: %.3f %.3f %.3f\n", stp->st_min[0], stp->st_min[1], stp->st_min[2]);
-  // printf("  BVH Bounding Box Max: %.3f %.3f %.3f\n", stp->st_max[0], stp->st_max[1], stp->st_max[2]);
   VMOVE( stp->st_min, model_bbox.min.values );
   VMOVE( stp->st_max, model_bbox.max.values );
-
-  // VMOVE(stp->st_min, tie->amin);
-  // VMOVE(stp->st_max, tie->amax);
 
   /* zero thickness will get missed by the raytracer */
   BBOX_NONDEGEN(stp->st_min, stp->st_max, rtip->rti_tol.dist);
 
-  // VMOVE(stp->st_center, tie->mid);
   // Calculate center of bounding box
   VMOVE( stp->st_center, model_bbox.get_center().values );
 
@@ -274,7 +222,9 @@ int bvh_build( struct soltab *stp, struct rt_bot_internal *bot_ip, struct rt_i *
   return 0;
 }
 
+#ifndef MAXHITS
 #define MAXHITS 128
+#endif
 
 struct hitdata_s {
     int nhits;
@@ -318,7 +268,6 @@ hitfunc(struct tie_ray_s *ray, struct tie_id_s *id, struct tie_tri_s *UNUSED(tri
    * ray direction. */
   VMOVE(tsp->tri_N, id->norm);
   hp->hit_vpriv[X] = VDOT(tsp->tri_N, ray->dir);
-  tsp->tri_normals = tsp->tri_N;
 
   /* Of the hit_vpriv assignments added in commit 50164, only hit_vpriv[X]
    * was based on initialized calculations.  Rather than leaving the other
@@ -343,6 +292,9 @@ int  bvh_shot(struct soltab *stp, struct xray *rp, struct application *ap, struc
   static int hit_number = 0;
   shot_number++;
 
+  auto ID = bu_parallel_id() - 1;
+  constexpr size_t invalid_prim_id = std::numeric_limits<size_t>::max();
+
   using Scalar  = Float;
   using Vec3    = bvh::v2::Vec<Scalar, 3>;
   using BBox    = bvh::v2::BBox<Scalar, 3>;
@@ -351,7 +303,6 @@ int  bvh_shot(struct soltab *stp, struct xray *rp, struct application *ap, struc
   using Bvh     = bvh::v2::Bvh<Node>;
   using Ray     = bvh::v2::Ray<Scalar, 3, false>;
   using PtrTri  = BVH::PtrTri<Scalar, 3>;
-  constexpr size_t invalid_prim_id = std::numeric_limits<size_t>::max();
 
   struct bot_specific * bot = (bot_specific*)stp->st_specific;
   // nanort::BVHAccel<Float> * accel = (nanort::BVHAccel<Float>*) bot->nanort;
@@ -363,12 +314,13 @@ int  bvh_shot(struct soltab *stp, struct xray *rp, struct application *ap, struc
   struct tie_ray_s ray;
   int i;
   fastf_t dirlen;
+  struct hitdata_s hitdata;
 
 
   tie = (struct tie_s *)bot->tie;
 
-  // hitdata.nhits = 0;
-  // hitdata.rp = &ap->a_ray;
+  hitdata.nhits = 0;
+  hitdata.rp = &ap->a_ray;
   /* do not need to init 'hits' and 'ts', tracked by 'nhits' */
 
   /* small backout applied to ray origin */
@@ -376,11 +328,6 @@ int  bvh_shot(struct soltab *stp, struct xray *rp, struct application *ap, struc
   VSUB2(ray.pos, rp->r_pt, rp->r_dir);	/* step back one dirlen */
   VMOVE(ray.dir, rp->r_dir);
 
-  struct hitdata_s hitdata;
-  hitdata.nhits = 0;
-  hitdata.rp = rp;
-
-  bvh::v2::SmallStack<typename Bvh::Index, 128 > stack;
   Ray bvhray( ray.pos, ray.dir, rp->r_min, rp->r_max );
   Float hit_isect = std::numeric_limits<Float>::max();
   size_t hit_prim = -1;
@@ -392,74 +339,56 @@ int  bvh_shot(struct soltab *stp, struct xray *rp, struct application *ap, struc
     return ss.str();
   };
 
-  accel.bvh.template intersect<false, false>( bvhray, accel.bvh.get_root().index, stack, [&](size_t begin, size_t end) {
+  accel.stacks[ ID ].size = 0;
+  accel.bvh.template intersect<false, false>( bvhray, accel.bvh.get_root().index, accel.stacks[ ID ], [&](size_t begin, size_t end) {
     bool hit = false;
     for( int i = begin; i < end; ++i ) {
-      int j = i;
-      if constexpr( PERMUTE_PRIMS ) {
-        j = accel.bvh.prim_ids[i];
-      }
+      int j = PERMUTE_PRIMS ? i : accel.bvh.prim_ids[i];
 
       // HIT
       Float isect = 0;
-      auto uv = accel.tris[j].intersect( bvhray, isect, -std::numeric_limits<Float>::epsilon() );
+      auto uv = accel.tris[j].intersect( bvhray, bvhray.tmax, -std::numeric_limits<Float>::epsilon() );
       if( uv ) {
-        if( isect < hit_isect ) {
-          hit_prim = accel.tris[j].prim_id;
-          hit_isect = isect;
-        }
         hit = true;
-        // printf("Hit(%5d): %4d %.4f\n", shot_number, accel.tris[j].prim_id, isect );
-        // auto & [u, v] = *uv;
         tie_id_s tie_id;
         tie_ray_s tie_ray;
 
-        auto prim_id = accel.tris[j].prim_id;
+        std::tie( tie_id.alpha, tie_id.beta ) = *uv;
+
+        if( isect < hit_isect ) {
+          hit_prim = j;
+          hit_isect = bvhray.tmax;
+        }
+
         auto tri = accel.ptrTris[j];
-
-
-        // auto face0 = ((unsigned int*)bot->bot_facelist)[i + 0];
-        // auto face1 = ((unsigned int*)bot->bot_facelist)[i + 1];
-        // auto face2 = ((unsigned int*)bot->bot_facelist)[i + 2];
 
         Float *A = tri.p0.values; // &((Float*)bot->bot_facearray)[ face0 ];
         Float *B = tri.p1.values; // &((Float*)bot->bot_facearray)[ face1 ];
         Float *C = tri.p2.values; // &((Float*)bot->bot_facearray)[ face2 ];
-        // std::cerr << "Triangle " << j << ":" << std::endl;
-        // std::cerr << "\tA: " << vecString( A ) << std::endl;
-        // std::cerr << "\tB: " << vecString( B ) << std::endl;
-        // std::cerr << "\tC: " << vecString( C ) << std::endl;
 
         Float AC[3], AB[3];
         VSUB2(AC, C, A);
         VSUB2(AB, B, A);
         VCROSS(tie_id.norm, AC, AB);
         VUNITIZE( tie_id.norm );
-        tie_id.dist = isect;
+        tie_id.dist = bvhray.tmax;
 
         VMOVE(tie_ray.dir, bvhray.dir.values );
         VMOVE(tie_ray.pos, bvhray.org.values );
 
         struct tri_specific * ts = hitdata.ts + hitdata.nhits;
 
-        ts->tri_normals = ts->tri_N;
+        ts->tri_normals = nullptr; // ts->tri_N;
 
         auto hfret = hitfunc( &tie_ray, &tie_id, nullptr, &hitdata );
         if( hfret != nullptr ) {
           throw std::runtime_error("Too many hits!!!");
         }
-        break;
       }
 
     }
     return hit;
   });
-
-
-  // if( hit_prim != -1 ) {
-  //   printf("Found hit(%4d). T = %.4f  ID=%5d\n", shot_number, hit_isect, hit_prim );
-  //   printf("Hit number %d\n", ++hit_number );
-  // }
 
   if( hitdata.nhits > 0 ) {
     for (i = 0; i < hitdata.nhits; i++)
@@ -472,9 +401,6 @@ int  bvh_shot(struct soltab *stp, struct xray *rp, struct application *ap, struc
         return h1.hit_dist < h2.hit_dist;
     });
 
-    // printf("Closest Hit: %.3f : %.3f %.3f %.3f\n", hitdata.hits[0].hit_dist, hitdata.hits[0].hit_point[0]
-    //                                                                        , hitdata.hits[0].hit_point[1]
-    //                                                                        , hitdata.hits[0].hit_point[2] );
 
     return rt_bot_makesegs( hitdata.hits, hitdata.nhits, stp, rp, ap, seghead, nullptr );
   }
